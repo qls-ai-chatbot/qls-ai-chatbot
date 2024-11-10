56ebb6b8f54825017229f870cf4c00a233bdeae1
quangtu.le <quangtu.le@gmail.com>
2024-11-10 14:42:50 +0100

    separation promt with sugesstion

--- liferay/workspace/client-extensions/qls-ai-chatbot-etc-backend/src/main/java/com/github/qls/ai/chatbot/backend/service/StreamingAssistant.java
+++ liferay/workspace/client-extensions/qls-ai-chatbot-etc-backend/src/main/java/com/github/qls/ai/chatbot/backend/service/StreamingAssistant.java
@@ -6,7 +6,7 @@ import dev.langchain4j.service.UserMessage;
 import reactor.core.publisher.Flux;
 
 public interface StreamingAssistant {
-
+	//@SystemMessage(fromResource = "system-message-with-suggestion.st")
 	@SystemMessage(fromResource = "prompts/system-message.st")
 	Flux<String> chat(@MemoryId String memoryId, @UserMessage String message);
 
